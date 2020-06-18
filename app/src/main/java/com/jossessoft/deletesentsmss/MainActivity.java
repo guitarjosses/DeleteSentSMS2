8db404813b17430c37012f3f5ae6a6ec39c4fc42
Juan Bautista Josses Corrales <jjosses@inss.gob.ni>
2020-06-18 06:53:24 -0600

    tercer commit

--- app/src/main/java/com/jossessoft/deletesentsmss/MainActivity.java
+++ app/src/main/java/com/jossessoft/deletesentsmss/MainActivity.java
@@ -36,9 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 getPackageName());
         startActivityForResult(setSmsAppIntent, PERMISSION_REQUEST_SMS);
 
-            String defaultSmsApp = Telephony.Sms.getDefaultSmsPackage(getApplicationContext());
-            gDefaultSmsApp = defaultSmsApp;
-
         }
 
         s1 = (Switch) findViewById(R.id.switch1);
@@ -48,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean activo) {
 
                 if(activo){
+
+                    Intent setSmsAppIntent =
+                            new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+                    setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
+                            getPackageName());
+                    startActivityForResult(setSmsAppIntent, PERMISSION_REQUEST_SMS);
 
                     mProgramarTarea.run();
 
@@ -75,37 +78,42 @@ public class MainActivity extends AppCompatActivity {
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
 
-            int countSMS = 0;
+            int countSentSMSs = 0;
 
             Uri deleteUri = null;
             deleteUri = Telephony.Sms.CONTENT_URI;
 
             Cursor c = getApplicationContext().getContentResolver().query(deleteUri, null, null, null, null);
 
-            try {
+            int countSMSs = c.getCount();
 
-                while (c.moveToNext()) {
+            if(countSMSs > 0){
+                try {
 
-                    if (!c.getString(c.getColumnIndexOrThrow("type")).contains("1")) {
+                    while (c.moveToNext()) {
 
-                        countSMS = countSMS + 1;
+                        if (!c.getString(c.getColumnIndexOrThrow("type")).contains("1")) {
 
-                        String pid = c.getString(0);
-                        Uri.Builder uri = Telephony.Sms.CONTENT_URI.buildUpon().appendPath(pid);
-                        getApplicationContext().getContentResolver().delete(Uri.parse(uri.toString()),
-                                null, null);
+                            countSentSMSs = countSentSMSs + 1;
+
+                            String pid = c.getString(0);
+                            Uri.Builder uri = Telephony.Sms.CONTENT_URI.buildUpon().appendPath(pid);
+                            getApplicationContext().getContentResolver().delete(Uri.parse(uri.toString()),
+                                    null, null);
+                        }
+
                     }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }finally{
+                    c.close();
+                    Snackbar.make(findViewById(R.id.myRelativeLayout), countSentSMSs>1?countSentSMSs + " SMSs borrados": countSentSMSs + " SMS borrado",
+                            Snackbar.LENGTH_SHORT)
+                            .show();
 
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }finally{
-                c.close();
-                Snackbar.make(findViewById(R.id.myRelativeLayout), countSMS>1?countSMS + " SMSs borrados": countSMS + " SMS borrado",
-                        Snackbar.LENGTH_SHORT)
-                        .show();
-
             }
+
         }
     }
 
